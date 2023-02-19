f3f6dd1093f4a04b62cd3a06d645e2e6d428b7f8
AZH0823 <qoo9986360@gmail.com>
2023-02-20 00:50:24 +0800

    css modify

--- src/components/formIntro.vue
+++ src/components/formIntro.vue
@@ -1,7 +1,7 @@
 
 <script>
 export default {
-  setup(){
+  setup() {
     // scroll 動畫參數
     const easeInQuad = function (x) {
       return 1 - Math.pow(1 - x, 4);
@@ -9,7 +9,7 @@ export default {
 
     // scrolling 動畫手刻
     const scrolltoVeiew = () => {
-     
+
       const top = document.querySelector("#formInputer").getBoundingClientRect().top;
       const startPos = window.pageYOffset;
       const diff = top;
@@ -47,16 +47,15 @@ export default {
 }
 </script>
 
-<template>
-  <!-- <h1 class="p-1">第一區塊</h1> -->
+<template><!-- <h1 class="p-1">第一區塊</h1> -->
   <div class="intro fulid-container">
     <div class="block-container">
       <h2 class="white">There is no one</h2>
       <h2 class="white">who loves pain</h2>
       <button class="form-btn b-shadow" @click="scrolltoVeiew">Form</button>
       <div class="para-desc">
-        <img src="@/assets/images/sup01.svg" class="sup sup01" alt=""/>
-        <img src="@/assets/images/sup02.svg" class="sup sup02" alt=""/>
+        <img src="@/assets/images/sup01.svg" class="sup sup01" alt="" />
+        <img src="@/assets/images/sup02.svg" class="sup sup02" alt="" />
         <h5 class="para-icons p-1 bold">paragraph</h5>
         <ul>
           <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
@@ -73,7 +72,6 @@ export default {
     </div>
     <!-- <img src="@/assets/images/seaWave.png" alt=""> -->
   </div>
- 
 </template>
 
 <style lang="scss" >
@@ -84,61 +82,65 @@ export default {
 @import "~@/assets/scss/base/font.scss";
 @import "~@/assets/scss/mixin/mixin.scss";
 
-.intro.fulid-container{
+.intro.fulid-container {
 
   background-color: $primary-1;
-  color:$primary-1;
+  color: $primary-1;
   // padding:70px 0;
   min-height: 687px;
   border: 1px solid transparent;
 
-  background-image: 
-                    url('~@/assets/images/Linear01.png'),
-                    url('~@/assets/images/Linear02.png'),
-                    url('~@/assets/images/Linear03.png'),
-                    url('~@/assets/images/seaWave.png');
-  background-position:20% 20%, 75% 30%, 45% 85%, 0% 101%;
-  background-repeat: repeat no-repeat, repeat no-repeat ,repeat no-repeat;
-  background-size: 125%, 125%, 125%, auto; 
+  background-image:
+    url('~@/assets/images/Linear01.png'),
+    url('~@/assets/images/Linear02.png'),
+    url('~@/assets/images/Linear03.png'),
+    url('~@/assets/images/seaWave.png');
+  background-position: 20% 20%, 75% 30%, 45% 85%, 0% 101%;
+  background-repeat: repeat no-repeat, repeat no-repeat, repeat no-repeat;
+  background-size: 125%, 125%, 125%, auto;
   animation: moveBgPosition 60s infinite linear alternate;
 
   @keyframes moveBgPosition {
-      from {
-        background-position-x: 20%,75%,45%,0%;
-      }
-  
-      to {
-        background-position-x: 195%,210%,135%,220%;
-      }
+    from {
+      background-position-x: 20%, 75%, 45%, 0%;
+    }
+
+    to {
+      background-position-x: 195%, 210%, 135%, 220%;
+    }
   }
 
   .para-desc {
     padding: 24px 16px;
 
-    background:$primary-3;
+    background: $primary-3;
     border: 2px solid $primary-2;
     border-radius: 20px;
 
     position: relative;
-    
+
     // sup img
-    .sup{
+    .sup {
       position: absolute;
-      &.sup01{
+
+      &.sup01 {
         right: 0%;
         top: -10%;
       }
-      &.sup02{
+
+      &.sup02 {
         left: 5%;
         bottom: -15%;
-        
+
       }
     }
-    ul{
+
+    ul {
       // outline: 2px red solid;
       padding: 0 16px;
       margin: 0 auto;
-      li{
+
+      li {
         // @extend .title-16;
         list-style-type: decimal;
         list-style-position: outside;
@@ -150,20 +152,23 @@ export default {
           text-align: inherit;
         }
       }
-      li::marker{
+
+      li::marker {
         display: block;
         margin-left: 20px;
       }
     }
-    h5.para-icons{
+
+    h5.para-icons {
       padding: 20px;
       display: inline-block;
       position: relative;
       font-weight: 700;
       line-height: 22.5px;
     }
+
     .para-icons::before,
-    .para-icons::after{
+    .para-icons::after {
       content: '';
       position: absolute;
       background-size: 30px 30px;
@@ -174,16 +179,17 @@ export default {
       background-repeat: no-repeat;
 
     }
-    .para-icons::before{
+
+    .para-icons::before {
       left: -7.5%;
     }
+
     .para-icons::after {
       right: -7.5%;
     }
 
-    
+
   }
 
-    
-}
-</style>
+
+}</style>
